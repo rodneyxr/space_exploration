e622c5fe9ff4f7d9f1486a82001a0429eb35ba39
mikeschap <mikeschappel@gmail.com>
2015-11-10 21:43:01 -0600

    Accretion

---
+++ Assets/Scripts/Accretion.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accretion : MonoBehaviour
+{
+
+	public ParticleSystem system;
+	public GameObject heavenlyBody;
+	public float dissolveSpeed;
+	public float showSpeed, showEmit;
+	[SerializeField]
+	private bool
+		start, finish;
+
+	void Start ()
+	{
+		start = false;
+		finish = false;
+	}
+
+	void FixedUpdate ()
+	{
+		showSpeed = system.startSpeed;
+		showEmit = system.emissionRate;
+		if (start) {
+			system.startSize = Mathf.Lerp (system.startSize, 0, Time.deltaTime / (dissolveSpeed * 5));
+			if (system.startSpeed > .2) {
+				system.startSpeed = Mathf.Lerp (system.startSpeed, 0, Time.deltaTime / dissolveSpeed);
+			} else {
+				start = false;
+				finish = true;
+				heavenlyBody.GetComponent<FadeIn> ().startFadeIn ();
+			}
+		} else if (finish) {
+			system.startSize = Mathf.Lerp (system.startSize, 0, Time.deltaTime / (dissolveSpeed * 2));
+			if (system.emissionRate > .2) {
+				system.emissionRate = Mathf.Lerp (system.emissionRate, 0, Time.deltaTime / dissolveSpeed);
+			} else {
+				system.emissionRate = 0;
+				this.enabled = false;
+			}
+		}
+	}
+
+	public void startAccrete ()
+	{
+		start = true;
+	}
+
+}
---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FadeIn : MonoBehaviour
+{
+
+	public Color color;
+	public List<Material> materials;
+	public List<Light> lights;
+	private bool start;
+
+	void Start ()
+	{
+		start = false;
+		foreach (Light light in lights)
+			light.intensity = 0f;
+		foreach (Material mat in materials) {
+			mat.color = Color.black;
+			if (mat.HasProperty ("_ColorTint"))
+				mat.SetColor ("_ColorTint", Color.black);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if (start) {
+			foreach (Light light in lights)
+				light.intensity = Mathf.Lerp (light.intensity, 1f, Time.deltaTime);
+			foreach (Material mat in materials) {
+				mat.color = Color.Lerp (mat.color, color, Time.deltaTime);
+				if (mat.HasProperty ("_ColorTint"))
+					mat.SetColor ("_ColorTint", Color.Lerp (mat.GetColor("_ColorTint"), color, Time.deltaTime));
+			}
+		}
+	}
+
+	public void startFadeIn ()
+	{
+		start = true;
+	}
+}
